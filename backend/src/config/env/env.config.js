0190d50c225445e1ca1f931caf558294ac87bef6
m3dev4 <m3dev4@gmail.com>
2025-05-04 23:23:36 +0000

    DevsOps: Configuration du pipeline avec github action et les fichiers Dockers🐬

--- backend/src/config/env/env.config.js
+++ backend/src/config/env/env.config.js
@@ -6,6 +6,8 @@ dotenv.config();
 const appConfig = () => ({
   NODE_ENV: getEnv("NODE_ENV", "development"),
   PORT: getEnv("PORT", 3000),
+  DOCKERHUB_USERNAME: getEnv("DOCKERHUB_USERNAME"),
+  DOCKERHUB_TOKEN: getEnv("DOCKERHUB_TOKEN"),
 });
 
 export const envConfig = appConfig();
